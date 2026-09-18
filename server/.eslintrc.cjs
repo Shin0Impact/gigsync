405be111ff37f992c8ca6a714ec7502b585384d9
Maher <maherbathish@gmail.com>
2026-09-19 00:01:01 +0300

    eslint fix and schema matching

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
   ignorePatterns: ['dist', 'node_modules'],
 };
