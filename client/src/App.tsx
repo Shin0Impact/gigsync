e294071143483c0b44877ea8444cd9d4ff9d06e7
jinadabd <jenad.abd@campus.technion.ac.il>
2026-09-21 22:49:22 +0300

    populate routes with mock pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,4 @@ function App() {
 	return <AppRoutes />;
 }
 
-// function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route path="/socket-test" element={<SocketTestPage />} />
-//     </Routes>
-//   );
-// }
-
 export default App;
